41ccf362cc311500f424a3e613b3f6c2a8cb005e
Pedro Henrique <113939119+PedroVaillant@users.noreply.github.com>
2023-01-16 00:54:02 -0300

    Add files

--- src/components/Carrossel.jsx
+++ src/components/Carrossel.jsx
@@ -1,46 +1,16 @@
-// import React from 'react';
-// import 'bootstrap/dist/css/bootstrap.css'
-// import Carousel from 'react-bootstrap/Carousel'
-
-// function Carrossel() {
-//     return (
-//       <div style={{display: 'block', width: '800', padding: 30}}>
-//         <Carousel>
-//           <Carousel.Item>
-//             <img
-//               src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
-//               alt="Image One"
-//             />
-//           </Carousel.Item>
-//           <Carousel.Item>
-//             <img
-//               src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
-//               alt="Image Two"
-//             />
-//           </Carousel.Item>
-//         </Carousel>
-//       </div>
-//     );
-//   }
-
-//   export default Carrossel
-
 import { Carousel } from 'react-carousel-minimal';
 
 function Carrossel() {
  const data = [
     {
-      image: "./img/teste1.png",
+      image: "./img/Teste1.png",
     },
     {
-      image: "./img/teste2.png",
+      image: "./img/Teste2.png",
     },
     {
-      image: "./img/teste1.png",
+      image: "./img/Teste3.png",
     },
-    {
-      image: "./img/teste2.png",
-    }
     // {
     //   image: "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg",
     //   caption: "Darjeeling"
