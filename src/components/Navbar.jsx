0fa14dc9d6d44fc96a9db2b73afdb89294c566be
Kay <114102129+kaymattos@users.noreply.github.com>
2023-01-18 16:34:35 -0300

    Login
    Adicionando rotas, página e css referente ao login.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,41 @@
-import { Link } from 'react-router-dom'
-import { BiSearchAlt2 } from 'react-icons/bi'
+import { Link } from "react-router-dom";
+import { BiSearchAlt2 } from "react-icons/bi";
 
 const Navbar = () => {
-    return (
-        <nav id="navbar">
-            <Link to="/">
-                <div className='reactlibmarca'>
-                    <h2>
-                        <img src="./img/icon.png" width="85" height="80" />
-                        <br />
-                        React Books
-                    </h2>
-                </div>
+  return (
+    <nav id="navbar">
+      <Link to="/">
+        <div className="reactlibmarca">
+          <h2>
+            <img src="./img/icon.png" width="85" height="80" />
+            <br />
+            React Books
+          </h2>
+        </div>
+      </Link>
+      <div className="navbaropcoes">
+        <ul>
+          <Link to="/livros">
+            <li>Livros</li>
+          </Link>
+          <Link to="/sobre">
+            <li>Sobre nós</li>
+          </Link>
+          <li>
+            <input type="search" placeholder="Busque por um livro" />
+            <button type="subimit">
+              <BiSearchAlt2 />
+            </button>
+          </li>
+          <li>
+            <Link to="/login">
+              <li>LOGIN</li>
             </Link>
-            <div className='navbaropcoes'>
-                <ul>
-                    
-                    <Link to="/livros"><li>Livros</li></Link>
-                    <Link to="/sobre"><li>Sobre nós</li></Link>
-                    <li><input type="search" placeholder='Busque por um livro'/><button type='subimit'><BiSearchAlt2 /></button></li>
-                </ul>
-            </div>
-        </nav>
-    )
-}
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
