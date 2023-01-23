39e6b0d225e351d3aa96a3f7da3f242dd7542d4b
Pedro Henrique <113939119+PedroVaillant@users.noreply.github.com>
2023-01-23 16:30:22 -0300

    Add files via upload

--- src/model/API.jsx
+++ src/model/API.jsx
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://m3bibliotecadb.onrender.com",
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 export default API;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,51 @@
-import Carrossel from "../components/Carrossel"
+import { useState, useEffect } from "react";
 
-const Home = () => {
-    return (
-        <div>
-            <div>
-                <Carrossel />
+import Carrossel from "../components/Carrossel";
+import API from "../model/API";
+
+import styles from "../styles/LivrosCatalogo.module.css";
+
+const CatalogoLivros = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const response = await API.get("/Livrarias");
+
+      const data = response.data;
+
+      setPosts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <div>
+      <Carrossel />
+      <br />
+
+      <div className={styles.catalogo}>
+        {posts.length === 0 ? (
+          <div className={styles.loading}></div>
+        ) : (
+          posts.map((post) => (
+            <div className={styles.livros} key={post.id}>
+              <img src={`${post.foto}`} className={styles.imgItem} />
+              <h2 className={styles.titulo}>{post.loja}</h2>
+              <h2 className={styles.titulo}>{post.endereço}</h2>
             </div>
-        </div>
-    )
-}
+          ))
+        )}
+      </div>
+      <br />
+      <br />
+    </div>
+  );
+};
 
-export default Home
+export default CatalogoLivros;
